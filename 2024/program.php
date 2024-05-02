aca7049694bb8fc2c06de1dd08b3372f48232616
Jeremy Clark <j.clark@concordia.ca>
2024-05-02 11:49:22 -0400

    2024 website

---
+++ 2024/program.php
@@ -0,0 +1,19 @@
+<?php
+require "common/include.php";
+startPage();
+includeBanner();
+includeMenu();
+?>  
+<div class="content">
+
+<div class="sdate">TBA</div>
+
+
+</div>
+
+<?php
+includeFooter();
+?>
+</div>
+</body>
+</html>
---
+++ 2024/registration.php
@@ -0,0 +1,24 @@
+<?php
+require "common/include.php";
+startPage();
+includeBanner();
+includeMenu();
+?> 
+<div class="content">
+
+<font color=#003366 size=+2><b>Registration</b></font>
+
+<!--
+<h3><a href="https://docs.google.com/forms/d/e/1FAIpQLSdRXvywn4sW53TTu1-TGEASlqpsDKQIcuoy1xh5Z02zk_ziBg/viewform?usp=sf_link">Click for registration form</a></h3>
+-->
+
+Coming soon. 
+
+</div>
+
+<?php
+includeFooter();
+?>
+</div>
+</body>
+</html>
