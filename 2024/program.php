17e54532be7224479f55c1904eea379bb685a1cb
Jeremy Clark <j.clark@concordia.ca>
2024-06-13 15:21:20 -0400

    program

--- 2024/program.php
+++ 2024/program.php
@@ -6,10 +6,116 @@ includeMenu();
 ?>  
 <div class="content">
 
-<div class="sdate">TBA</div>
+<!--<p>Schedule is tentative and subject to change.</p>-->
+
+<!------------------------Tuesday June 1----------------------------->
+
+<br><br>
+
+<div class="sdate">Wednesday, June 26 (Eastern Time)</div>
+<div class="rowbreak" style="height: 20px"></div>
+
+<p align="right">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/GMRfKDUWxzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</p><br/><br/>
 
 
+<div class="col-container">
+    <div class="scolumn-33">
+        <div class="stime">10:50 – 11:00</div>
+    </div>
+    <div class="scolumn-66" style="background-color: #b2f4f0; padding-top:0">
+        <div class="stitle">Opening remarks</div>
+    </div> 
 </div>
+
+<div class="rowbreak"></div>
+
+<!--<div class="col-container">
+    <div class="scolumn-33">
+        <div class="stime">11:15 – 12:15</div>
+    </div>
+    <div class="scolumn-66">
+        <div class="stitle">Keynote</div><br/> 
+        <div class="sstitle">Tentative (Otherwise we will start 1 hour later)</div>
+        <div class="ssstitle" style="border-bottom: 0px">Elaine Shi (Carnegie Mellon University)</div>
+        
+        <button class="collapsible-radius">Learn More</button>
+        <div class="content1">
+            <p><b>Bio:</b> </p>
+            <p><b>Abstract:</b> </p>
+        </div>
+    </div> 
+</div>-->
+
+<div class="rowbreak"></div>
+
+<div class="col-container">
+    <div class="scolumn-33">
+        <div class="stime">11:00 – 12:00</div>
+    </div>
+    <div class="scolumn-66">
+        <div class="stitle">Keynote</div><br/> 
+        <div class="ssstitle" style="border-bottom: 0px">Xiang Fu (Hofstra University)</div>
+    </div> 
+</div>
+
+<div class="rowbreak"></div>
+
+<div class="col-container">
+    <div class="scolumn-33">
+        <div class="stime">12:00 – 13:00</div>
+    </div>
+    <div class="scolumn-66">
+        <div class="stitle">Keynote</div><br/> 
+        <div class="ssstitle" style="border-bottom: 0px">TBD</div>
+    </div> 
+</div>
+
+<div class="rowbreak"></div>
+
+<div class="col-container">
+    <div class="scolumn-33">
+        <div class="stime">13:00 – 13:15</div>
+    </div>
+    <div class="scolumn-66">
+        <div class="stitle">Break</div><br/> 
+    </div> 
+</div>
+
+<div class="rowbreak"></div>
+
+
+<div class="col-container">
+    <div class="scolumn-33">
+        <div class="stime">13:15 – 14:45</div>
+    </div>
+    <div class="scolumn-66">
+        <div class="stitle">Canadian Scholars</div><br/> 
+        <div class="ssstitle" style="border-bottom: 0px">Lili Wei (McGill University)</div>
+    </div> 
+    <div class="scolumn-66">
+        <div class="ssstitle" style="border-bottom: 0px">Majid Khabbazian (University of Alberta)</div>
+    </div> 
+    <div class="scolumn-66">
+        <div class="ssstitle" style="border-bottom: 0px">Mariam Humayun (University of Ottawa)</div>
+    </div> 
+</div>
+
+<div class="rowbreak"></div>
+
+<div class="col-container">
+    <div class="scolumn-33">
+        <div class="stime">15:00 – 16:00</div>
+    </div>
+    <div class="scolumn-66">
+        <div class="stitle">Short Contributed Talks</div><br/> 
+        <div class="ssstitle" style="border-bottom: 0px">TBA</div>
+    </div> 
+</div>
+
+
+<br>
 
 <?php
 includeFooter();
